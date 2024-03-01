616425b69df1482c1379a581f15daf135c378b15
Sudeep Yadav <sudeeprky@gmail.com>
2024-02-29 19:11:55 -0500

    first commit

---
+++ src/main/java/com/exampletest/springbootdocker/MyController.java
@@ -0,0 +1,15 @@
+package com.exampletest.springbootdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MyController {
+    @GetMapping("/test")
+    public String testing(){
+
+        return "testing";
+    }
+
+}
